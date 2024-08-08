e465ed6ec0a7c1b570c7cfac37a78c899009dcfb
Harsha <hmuppara@asu.edu>
2024-08-08 12:13:46 +0530

    initial setup - font, style, layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import Dashboard from './view/Dashboard';
+
+function App() {
+  console.log(4 + 2);
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/view/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './dashboard.scss';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <header>
+        <h1>63,179.71</h1>
+        <p>+ 2,161.42 (3.54%)</p>
+      </header>
+      <p>tabs</p>
+      <p>graph</p>
+    </div>
+  );
+};
+
+export default Dashboard;
